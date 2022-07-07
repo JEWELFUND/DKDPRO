7538e91178eb995797b7d556cc07f83a146835d5
JEW2L -XD <99085273+JEWELFUND@users.noreply.github.com>
2022-07-07 11:50:15 +0100

    Update DKD.py

--- DKD.py
+++ DKD.py
@@ -2,12 +2,11 @@ import os,time,rich
 from rich.panel import Panel as nel
 from rich import print as cetak
  
-__alvino__ganteng__ = '\t[yellow]•[white] I Am [green] The ONLY[white] JEWEL CYBER TECH [yellow] •[white]'
+__alvino__ganteng__ = '\t[yellow]•[white] I Am [green] The ONLY[white] JEWEL🇳🇬[yellow] •[white]'
 cetak(nel(__alvino__ganteng__));time.sleep(1)
  
 if __name__ == "__main__":
         try:
-                __import__("DKD").login()
+                __import__("JAVIS").login()
         except Exception as e:
                 exit(str(e))
-
