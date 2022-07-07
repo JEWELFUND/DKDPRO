6a30d59a788f4a597f2ce9558aa8c62a6ee8d300
JEW2L -XD <99085273+JEWELFUND@users.noreply.github.com>
2022-07-07 12:41:53 +0100

    Update DKD1.py

--- DKD1.py
+++ DKD1.py
@@ -7,6 +7,6 @@ cetak(nel(__alvino__ganteng__));time.sleep(1)
  
 if __name__ == "__main__":
         try:
-                __import__("JAVIS").login()
+                __import__("DKD1").login()
         except Exception as e:
                 exit(str(e))
